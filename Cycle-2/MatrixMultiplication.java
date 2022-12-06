10c70208cca915269bb93c041a1c03dee280342a
Dennis Johns <99817065+djohns1011@users.noreply.github.com>
2022-12-06 20:32:47 +0530

    Update MatrixMultiplication.java

--- Cycle-2/MatrixMultiplication.java
+++ Cycle-2/MatrixMultiplication.java
@@ -28,10 +28,8 @@ class MatrixMultiplication {
         int mat3[][] = new int[r1][c2];
         for(int i = 0; i < r1; i++){
             for(int j = 0; j < c2; j++){
-                for(int k = 0; k < r2; k++){
-                    System.out.println(mat3[i][j] + " = " + mat3[i][j] +" + "+ mat1[i][k] + " * " + mat2[k][j]);
-                    mat3[i][j] += mat1[i][k] * mat2[k][j];;
-                    
+                for(int k = 0; k < r2; k++){    
+                    mat3[i][j] += mat1[i][k] * mat2[k][j];;                    
                 }
             }
         }
